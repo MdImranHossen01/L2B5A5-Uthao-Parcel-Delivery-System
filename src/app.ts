98547ef953bbf3af561792c4ed6c9be2eee09c64
JiaPixel <imranshuvo101@gmail.com>
2025-07-29 12:56:57 +0600

    user and erro setup complete

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,25 @@
+import cors from "cors";
 import express, { Request, Response } from "express";
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
+import { router } from "./app/routes";
 
-const app = express();
+const app = express()
 
-app.get("/",(req: Request, res: Response) => {
+app.use(express.json())
+app.use(cors())
+
+app.use("/api/v1", router)
+
+app.get("/", (req: Request, res: Response) => {
     res.status(200).json({
-        message: "Welcome to Uthao Parcel Delivery System API"
-    });
-});
+        message: "Welcome to Tour Management System Backend"
+    })
+})
 
-export default app;
+
+app.use(globalErrorHandler)
+
+app.use(notFound)
+
+export default app
---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,35 @@
+import { Types } from "mongoose";
+
+export enum Role {
+    SUPER_ADMIN = "SUPER_ADMIN",
+    ADMIN = "ADMIN",
+    RECEIVER = "RECEIVER",
+    SENDER = "SENDER",
+}
+
+
+export interface IAuthProvider {
+    provider: string;  // "Google", "Credential"
+    providerId: string;
+}
+
+export enum IsActive {
+    ACTIVE = "ACTIVE",
+    BLOCKED = "BLOCKED"
+}
+
+export interface IUser {
+    name: string;
+    email: string;
+    password?: string;
+    phone?: string;
+    picture?: string;
+    address?: string;
+    isDeleted?: string;
+    isActive?: IsActive;
+    isVerified?: string;
+    role: Role;
+    auths: IAuthProvider[]
+  
+   
+}
